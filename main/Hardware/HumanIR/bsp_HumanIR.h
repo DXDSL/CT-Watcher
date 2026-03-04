0eb7e358775a68fe557db7db4eeef1563786d65d
DSL <dxdsl@189.cn>
2026-03-04 22:42:58 +0800

    Introducing a dual-mode hybrid distribution network architecture

--- main/Hardware/HumanIR/bsp_HumanIR.h
+++ main/Hardware/HumanIR/bsp_HumanIR.h
@@ -1,19 +1,16 @@
 #ifndef _BSP_HUMANIR_H_
 #define _BSP_HUMANIR_H_
- 
-#include "sdkconfig.h"
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
 #include "freertos/queue.h"
 #include <inttypes.h>
-#include "sdkconfig.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
 #include "rom/ets_sys.h"
 #include "esp_system.h"
-#include "driver/gpio.h"
- 
+
 #define PIR_PIN    1
 
 void delay_ms(unsigned int ms);
